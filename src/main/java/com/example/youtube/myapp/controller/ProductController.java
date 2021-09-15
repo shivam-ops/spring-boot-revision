d24d1a8ee736bfd1958c38e8969f4293e38a39b7
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-09-15 21:58:25 +0530

    receiving a list of products

--- src/main/java/com/example/youtube/myapp/controller/ProductController.java
+++ src/main/java/com/example/youtube/myapp/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.example.youtube.myapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.youtube.myapp.entities.Product;
+
 
 @RestController
 public class ProductController {
@@ -18,5 +22,21 @@ public class ProductController {
 	public Product getProduct() {
 		Product myProduct = new Product(1, "A", 10);
 		return myProduct;
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getProducts() {
+		
+		List<Product> myProducts = new ArrayList<>(); 
+		
+		Product p1 = new Product(1, "A", 10);
+		Product p2 = new Product(1, "B", 20);
+		Product p3 = new Product(1, "C", 30);
+		
+		myProducts.add(p1);
+		myProducts.add(p2);
+		myProducts.add(p3);
+		
+		return myProducts;
 	}
 }
